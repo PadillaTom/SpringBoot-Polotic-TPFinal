7072fe77b6bc86fe087985efe3f6a6fc9a99d5bf
Tomas Padilla <padillatomasagustin@gmail.com>
2021-08-15 12:24:47 +0200

    CORS

--- src/main/java/com/padillatomas/tpfinal/controller/UsuarioController.java
+++ src/main/java/com/padillatomas/tpfinal/controller/UsuarioController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -14,6 +15,7 @@ import com.padillatomas.tpfinal.model.Usuario;
 import com.padillatomas.tpfinal.service.UsuarioService;
 
 
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @RestController
 @RequestMapping("/api/usuarios")
 public class UsuarioController {
