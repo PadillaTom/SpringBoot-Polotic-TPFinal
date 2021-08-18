8b1dbfdf6302800d8b578de6689c8191f0d6a6c0
Tomas Padilla <padillatomasagustin@gmail.com>
2021-08-18 18:34:38 +0200

    testing new query

--- src/main/java/com/padillatomas/tpfinal/repository/ReservaRepository.java
+++ src/main/java/com/padillatomas/tpfinal/repository/ReservaRepository.java
@@ -6,6 +6,6 @@ import org.springframework.data.jpa.repository.Query;
 import com.padillatomas.tpfinal.model.Reserva;
 
 public interface ReservaRepository extends JpaRepository<Reserva, Long> {
-	@Query("select * from reservas ORDER BY id DESC LIMIT 1")
+	@Query("select * from reservas ORDER BY reservaId DESC LIMIT 1")
 	Reserva lastItem();
 }
