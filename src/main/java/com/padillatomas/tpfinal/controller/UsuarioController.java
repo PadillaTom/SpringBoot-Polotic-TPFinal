ff5f0c74a86737ff9b9592d1617185ae1b4ffa14
Tomas Padilla <padillatomasagustin@gmail.com>
2021-08-24 10:13:01 +0200

    Testing EDIT

--- src/main/java/com/padillatomas/tpfinal/controller/UsuarioController.java
+++ src/main/java/com/padillatomas/tpfinal/controller/UsuarioController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -37,15 +38,29 @@ public class UsuarioController {
 		return usuarioService.fetchUsuarios();
 	}
 	
+	// ::: GET BY ID Usuario :::
+	@GetMapping("{id}")
+	public Usuario fetchById(@PathVariable("id") Usuario usu){
+		Long myId = usu.getUsuarioId();
+		return usuarioService.fetchById(myId);
+	}
+	
 	// ::: POST Usuarios :::
 	@PostMapping
 	public ResponseEntity<Usuario> saveUsuario(@RequestBody Usuario usuario){
 		return new ResponseEntity<Usuario>(usuarioService.saveUsuario(usuario), HttpStatus.CREATED);
 	}
 	
-	// ::: POST DELETE Usuarios :::
+	// ::: DELETE Usuarios :::
 	@DeleteMapping("{id}")
 	public ResponseEntity<String> borrarUsuario(@PathVariable("id") Usuario usuario){
+		usuarioService.eliminarUsu(usuario);
+		return new ResponseEntity<String> ("Usuario Borrado", HttpStatus.OK);
+	}
+	
+	// ::: PUT BY ID Usuario :::
+	@PutMapping("{id}")
+	public ResponseEntity<String> editarUsuario(@PathVariable("id") Usuario usuario){
 		usuarioService.eliminarUsu(usuario);
 		return new ResponseEntity<String> ("Usuario Borrado", HttpStatus.OK);
 	}
